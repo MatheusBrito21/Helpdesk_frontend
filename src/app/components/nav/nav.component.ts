7dd381313718ce6a7272c3ae11593199a00812e4
MatheusBrito21 <matheusb790@gmail.com>
2022-09-27 19:41:29 -0300

    Componente Tecnico Create

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -14,7 +14,7 @@ export class NavComponent implements OnInit {
   constructor(private router: Router, private authService: AutenticacaoService, private toast: ToastrService) { }
 
   ngOnInit(): void {
-    this.router.navigate(['home'])
+    this.router.navigate(['tecnico/create'])
   }
 
   logout(){
