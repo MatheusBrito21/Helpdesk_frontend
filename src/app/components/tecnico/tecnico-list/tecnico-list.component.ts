565c95bf5bf68adfa07045e967e1c58993bca778
MatheusBrito21 <matheusb790@gmail.com>
2022-09-27 18:26:38 -0300

    Finalizando Lista de Tecnicos

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -11,9 +11,9 @@ import { TecnicoService } from 'src/app/services/tecnico.service';
 })
 export class TecnicoListComponent implements OnInit {
   ELEMENT_DATA: Tecnico[] = []
-
   displayedColumns: string[] = ['id', 'nome', 'cpf', 'email','acoes'];
   dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
 
   constructor(private service: TecnicoService) { }
@@ -22,14 +22,19 @@ export class TecnicoListComponent implements OnInit {
     this.findAll(); 
   }
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
 
 
   findAll(){
     this.service.findAll().subscribe(resposta =>{
       this.ELEMENT_DATA = resposta;
       this.dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
     })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
